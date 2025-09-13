9ad71c9965b07d78afea4e32c2f5cbeff786cdd6
isa851 <isa904363@gmail.com>
2025-09-13 18:22:55 +0600

    fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import { ShoppingCart, Search, Store } from 'lucide-react';
-import './Header.scss';
+import React from "react";
+import { Search } from "lucide-react";
+import "./Header.scss";
+import { FaShopify } from "react-icons/fa";
+import { FaShoppingBasket } from "react-icons/fa";
 
-const Header = ({ cartItemsCount, onCartClick, searchTerm, onSearchChange }) => {
+const Header = ({
+  cartItemsCount,
+  onCartClick,
+  searchTerm,
+  onSearchChange,
+}) => {
   return (
     <header className="header">
       <div className="header__container">
         <div className="header__content">
           <div className="header__logo">
             <div className="header__logo-icon">
-              <Store className="header__logo-svg" />
+              <FaShopify className="header__logo-svg" />
             </div>
             <span className="header__logo-text">SHOPISKO</span>
           </div>
 
-    
           <div className="header__search">
             <div className="header__search-wrapper">
               <div className="header__search-icon">
@@ -32,13 +38,11 @@ const Header = ({ cartItemsCount, onCartClick, searchTerm, onSearchChange }) => 
 
           <button onClick={onCartClick} className="header__cart">
             <div className="header__cart-content">
-              <ShoppingCart className="header__cart-icon" />
+              <FaShoppingBasket className="header__cart-icon" />
               <span className="header__cart-text">Корзина</span>
             </div>
             {cartItemsCount > 0 && (
-              <span className="header__cart-badge">
-                {cartItemsCount}
-              </span>
+              <span className="header__cart-badge">{cartItemsCount}</span>
             )}
           </button>
         </div>
